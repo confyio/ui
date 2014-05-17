672d9eddbbb7cb3496f31abc151055e127441f7d
Benjamin Alijagić <ben@wivern.co>
2014-05-17 21:44:01 +0530

    Started working on landing page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,6 @@ import Env from 'confy/models/env';
 
 export default Ember.Route.extend({
   model: function() {
-    window.ORG = 'confy';
-    window.PROJECT = 'main'
-    return Env.find('staging');
+    return;
   }
 });
