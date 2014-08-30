7f7030c9fc99479ed3dbc4be7974ef627de491ef
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-08-30 23:15:01 +0530

    Moved navbar into it's own view

--- app/routes/pages/landing.js
+++ app/routes/pages/landing.js
@@ -1,6 +1,9 @@
 import PagesLandingView from 'confy/views/pages/landing';
+import PagesNavbarView from 'confy/views/pages/navbar';
 
 export default function () {
   $('body').attr('class', 'landing');
+
   React.renderComponent(PagesLandingView(), $('#wrap .row')[0]);
+  React.renderComponent(PagesNavbarView(), $('.navbar-collapse')[0]);
 };
---
+++ app/views/pages/navbar.jsx
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+
+export default React.createClass({
+  render: function () {
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#">Benefits</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Signup</a></li>
+      </ul>
+    );
+  }
+});
