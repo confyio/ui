647e82c79cd4d2994a0ad02253405c9785c8318e
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-10-15 01:58:42 +0200

    Added project_team model

--- app/helpers/projects.js
+++ app/helpers/projects.js
@@ -1,5 +1,7 @@
 import Project from 'confy/models/project';
+import ProjectTeam from 'confy/models/project_team';
 import ProjectCollection from 'confy/collections/project';
+import TeamCollection from 'confy/collections/team';
 
 var ProjectsHelper = {};
 
@@ -13,5 +15,14 @@ ProjectsHelper.list = function (callback) {
     }
   });
 };
+
+ProjectsHelper.teams = function (callback) {
+  new ProjectTeam().fetch({
+    success: function (child, data) {
+      window.project_teams = new TeamCollection(data);
+      return callback();
+    }
+  });
+}
 
 export default ProjectsHelper;
