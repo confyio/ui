8cd5484368d8f3ea5b2ac6e127da2f543ebd3063
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-10-16 01:58:03 +0200

    Finised listing access granted teams list

--- app/views/projects/info.jsx
+++ app/views/projects/info.jsx
@@ -9,6 +9,36 @@ export default React.createClass({
       <div>
         <ProjectsListView />
         <MainView type="Project" header={window.project.get('name')}>
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <td>Team Name</td>
+                <td>Actions</td>
+              </tr>
+            </thead>
+            <tbody>
+              {window.access.map(function (team) {
+                return (
+                  <tr>
+                    <td>
+                      <a href={team.get('link')}>{team.get('name')}</a>
+                    </td>
+                    <td>
+                      <button type="button" className="btn btn-danger" disabled={team.get('id') == 'owners' ? 'disabled' : ''}>Revoke Access</button>
+                    </td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td>
+                  <input id="grant-access" />
+                </td>
+                <td>
+                  <button type="button" className="btn btn-success">Grant Access</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </MainView>
       </div>
     );
