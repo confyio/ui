97e3a22b74954439786cad307943a2a5433e48d0
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-12-21 05:29:55 +0530

    Finished sidebar button design

--- app/views/sidebar.jsx
+++ app/views/sidebar.jsx
@@ -1,16 +1,12 @@
 /** @jsx React.DOM */
 
-import ProjectsCreateButtonView from 'confy/views/projects/button-create';
-import TeamsCreateButtonView from 'confy/views/teams/button-create';
-import EnvsCreateButtonView from 'confy/views/envs/button-create';
+import SideButtonView from 'confy/views/sidebutton';
 
 export default React.createClass({
   render: function () {
     return (
       <div className="sidebar">
-        <TeamsCreateButtonView type={this.props.type} />
-        <EnvsCreateButtonView type={this.props.type} />
-        <ProjectsCreateButtonView type={this.props.type} />
+        <SideButtonView type={this.props.type} />
         <ul className="nav nav-sidebar">
           {this.props.children}
         </ul>
