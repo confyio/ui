767ba89c4fa10865c4834799205525cdcf723b32
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-12-24 01:19:17 +0530

    Creating environment finished

--- app/routes/envs/view.js
+++ app/routes/envs/view.js
@@ -1,18 +1,22 @@
 import ProjectsViewRoute from 'confy/routes/projects/view';
 import EnvsInfoView from 'confy/views/envs/info';
+import EnvsHelper from 'confy/helpers/envs';
 
 export default function (org, project, env, callback) {
   var self = this;
 
   ProjectsViewRoute(org, project, function () {
-    if (window.env && window.env.get('id') == env) {
+    EnvsHelper.list(function () {
+
+      if (window.env && window.env.get('id') == env) {
+        if (callback) return callback();
+      }
+
+      window.env = window.envs.findWhere({ id: env });
+
       if (callback) return callback();
-    }
 
-    window.env = window.envs.findWhere({ id: env });
-
-    if (callback) return callback();
-
-    React.render(EnvsInfoView({}), $('#wrap')[0]);
+      React.render(EnvsInfoView({}), $('#wrap')[0]);
+    });
   });
 };
--- app/views/envs/create.jsx
+++ app/views/envs/create.jsx
@@ -2,9 +2,42 @@
 
 import MainView from 'confy/views/main';
 import ProjectsListView from 'confy/views/projects/list';
+import Env from 'confy/models/env';
+import ValidationHelper from 'confy/helpers/validation';
+import ValidationView from 'confy/views/validation';
 
 export default React.createClass({
-  handleSubmit: function () {
+  getInitialState: function () {
+    return ValidationHelper(['name', 'description']);
+  },
+  handleSubmit: function (e) {
+    var self = this;
+    e.preventDefault();
+
+    var env = new Env({
+      name: this.refs.name.getDOMNode().value.trim(),
+      description: this.refs.description.getDOMNode().value.trim()
+    });
+
+    env.on('invalid', function (model, errs) {
+      self.setState(ValidationHelper(self.state, errs, model, 'Environment'));
+    });
+
+    env.save({}, {
+      success: function (model, response) {
+        delete window.envs;
+
+        window.App.navigate(model.get('link'), {
+          trigger: true
+        });
+      },
+      error: function (model, response) {
+        if (response.status == 422) {
+          self.setState(ValidationHelper(self.state, response.responseJSON.errors, model, 'Environment'));
+        }
+      }
+    });
+
     return;
   },
   render: function () {
@@ -13,13 +46,15 @@ export default React.createClass({
         <ProjectsListView />
         <MainView header="Create Environment">
           <form role="form" onSubmit={this.handleSubmit}>
-            <div className="form-group">
+            <div className={this.state.name.className}>
               <label>Name</label>
-              <input className="form-control" placeholder="Enter environment name" />
+              <input className="form-control" placeholder="Enter environment name" ref="name" defaultValue={this.state.name.value} />
+              <ValidationView message={this.state.name.message} />
             </div>
-            <div className="form-group">
+            <div className={this.state.description.className}>
               <label>Description</label>
-              <input className="form-control" placeholder="Enter environment description" />
+              <input className="form-control" placeholder="Enter environment description" ref="description" defaultValue={this.state.description.value} />
+              <ValidationView message={this.state.description.message} />
             </div>
             <button type="submit" className="btn btn-default">Create Environment</button>
           </form>
