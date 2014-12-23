b84a8112e57dde1f958fd7489cad648209bf30f3
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-12-23 21:32:29 +0530

    Do not have a button to edit default environment

--- app/views/envs/button-update.jsx
+++ app/views/envs/button-update.jsx
@@ -4,7 +4,9 @@ import DummyView from 'confy/views/dummy';
 
 export default React.createClass({
   render: function () {
-    if (this.props.type == 'Environment' && window.env) {
+    var isOwner = (window.user.get('username') == window.org.get('owner'));
+
+    if (this.props.type == 'Environment' && window.env && window.env.get('id') != 'production' && isOwner) {
       return (
         <a className="btn btn-edit" href={window.env.get('link') + '/_update'}>
           <i className="fa fa-edit"></i>
