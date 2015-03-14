1a9dbf10a38fb20b0357e192e9449c504772756e
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-03-14 19:03:12 +0530

    Added loader

--- app/views/elements/loading.jsx
+++ app/views/elements/loading.jsx
@@ -4,7 +4,19 @@ export default React.createClass({
   render: function () {
     return (
       <div className="content">
-        <div className="loading"></div>
+        <div id="loader">
+          <div id="loader-out" className="box">
+            <div id="loader-in" className="box"></div>
+            <div className="ballbox box">
+              <div id="first-in" className="ball"></div>
+              <div id="second-in" className="ball"></div>
+            </div>
+          </div>
+          <div className="ballbox box">
+            <div id="first-out" className="ball"></div>
+            <div id="second-out" className="ball"></div>
+          </div>
+        </div>
       </div>
     );
   }
