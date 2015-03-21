616972268e0ee0cd490e9c54957b16aaf966e3c3
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2015-03-21 14:01:12 +0530

    Finished code samples on the homepage

--- app/app.js
+++ app/app.js
@@ -4,4 +4,19 @@ window.App = new Router();
 
 Backbone.history.start();
 
+// Landing page code samples
+jQuery('#usage-tabs a').click(function (e) {
+  e.preventDefault();
+
+  var lang = jQuery(this).attr('value');
+
+  jQuery('#usage-tabs li').removeClass('active');
+  jQuery('#usage #package div').removeClass('active');
+  jQuery('#usage #code div').removeClass('active');
+
+  jQuery('#usage-tabs #' + lang).addClass('active');
+  jQuery('#usage #package #' + lang).addClass('active');
+  jQuery('#usage #code #' + lang).addClass('active');
+});
+
 export default window.App;
